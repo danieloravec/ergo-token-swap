8485798b4d538a5d930cdc96574f84854d4aa296
Daniel Oravec <danoravec@gmail.com>
2023-02-04 22:57:04 +0100

    Build simple tx using wasm works

--- web/ergo/transactions.ts
+++ web/ergo/transactions.ts
@@ -212,3 +212,115 @@ export async function buildTxWasmExample(
   console.log(`signed tx: ${signed_tx.to_json()}`);
 }
 /* eslint-enable */
+
+/* eslint-disable */
+export async function buildTxAndPartialSign(dappWallet: DappWallet) {
+  const bob_secret = Loader.Ergo.SecretKey.dlog_from_bytes(
+    Uint8Array.from(
+      Buffer.from(
+        '9e6616b4e44818d21b8dfdd5ea87eb822480e7856ab910d00f5834dc64db79b3',
+        'hex'
+      )
+    )
+  );
+  const alice_pk_bytes = Uint8Array.from(
+    Buffer.from(
+      'cd03c8e1527efae4be9868cea6767157fcccac66489842738efed0a302e4f81710d0',
+      'hex'
+    )
+  );
+  // Pay 2 Script address of a multi_sig contract with contract { alicePK && bobPK }
+  const multi_sig_address = Loader.Ergo.Address.from_testnet_str(
+    'JryiCXrc7x5D8AhS9DYX1TDzW5C5mT6QyTMQaptF76EQkM15cetxtYKq3u6LymLZLVCyjtgbTKFcfuuX9LLi49Ec5m2p6cwsg5NyEsCQ7na83yEPN'
+  );
+  const input_contract = Loader.Ergo.Contract.pay_to_address(multi_sig_address);
+  const input_box = new Loader.Ergo.ErgoBox(
+    Loader.Ergo.BoxValue.from_i64(Loader.Ergo.I64.from_str('1000000000')),
+    0,
+    input_contract,
+    Loader.Ergo.TxId.zero(),
+    0,
+    new Loader.Ergo.Tokens()
+  );
+  // create a transaction that spends the "simulated" box
+  const recipient = Loader.Ergo.Address.from_testnet_str(
+    '3WvsT2Gm4EpsM9Pg18PdY6XyhNNMqXDsvJTbbf6ihLvAmSb7u5RN'
+  );
+  const unspent_boxes = new Loader.Ergo.ErgoBoxes(input_box);
+  const contract = Loader.Ergo.Contract.pay_to_address(recipient);
+  const outbox_value = Loader.Ergo.BoxValue.SAFE_USER_MIN();
+  const outbox = new Loader.Ergo.ErgoBoxCandidateBuilder(
+    outbox_value,
+    contract,
+    0
+  ).build();
+  const tx_outputs = new Loader.Ergo.ErgoBoxCandidates(outbox);
+  const fee = Loader.Ergo.TxBuilder.SUGGESTED_TX_FEE();
+  const change_address = Loader.Ergo.Address.from_testnet_str(
+    '3WvsT2Gm4EpsM9Pg18PdY6XyhNNMqXDsvJTbbf6ihLvAmSb7u5RN'
+  );
+  const box_selector = new Loader.Ergo.SimpleBoxSelector();
+  const target_balance = Loader.Ergo.BoxValue.from_i64(
+    outbox_value.as_i64().checked_add(fee.as_i64())
+  );
+  const box_selection = box_selector.select(
+    unspent_boxes,
+    target_balance,
+    new Loader.Ergo.Tokens()
+  );
+  const tx_builder = Loader.Ergo.TxBuilder.new(
+    box_selection,
+    tx_outputs,
+    0,
+    fee,
+    change_address
+  );
+  const tx = tx_builder.build();
+  const tx_data_inputs = Loader.Ergo.ErgoBoxes.from_boxes_json([]);
+  const block_headers = generate_block_headers();
+  const pre_header = Loader.Ergo.PreHeader.from_block_header(
+    block_headers.get(0)
+  );
+  const ctx = new Loader.Ergo.ErgoStateContext(pre_header, block_headers);
+  const sks_bob = new Loader.Ergo.SecretKeys();
+  sks_bob.add(bob_secret);
+  const wallet_bob = Loader.Ergo.Wallet.from_secrets(sks_bob);
+  const bob_hints = wallet_bob
+    .generate_commitments(ctx, tx, unspent_boxes, tx_data_inputs)
+    .all_hints_for_input(0);
+  const bob_known = bob_hints.get(0);
+  const bob_own = bob_hints.get(1);
+  let hints_bag = Loader.Ergo.HintsBag.empty();
+  hints_bag.add_commitment(bob_known);
+  const alice_tx_hints_bag = Loader.Ergo.TransactionHintsBag.empty();
+  alice_tx_hints_bag.add_hints_for_input(0, hints_bag);
+  const partial_signed = wallet_alice.sign_transaction_multi(
+    ctx,
+    tx,
+    unspent_boxes,
+    tx_data_inputs,
+    alice_tx_hints_bag
+  );
+  const real_propositions = new Loader.Ergo.Propositions();
+  const simulated_proposition = new Loader.Ergo.Propositions();
+  real_propositions.add_proposition_from_byte(alice_pk_bytes);
+  const bob_hints_bag = Loader.Ergo.extract_hints(
+    partial_signed,
+    ctx,
+    unspent_boxes,
+    tx_data_inputs,
+    real_propositions,
+    simulated_proposition
+  ).all_hints_for_input(0);
+  bob_hints_bag.add_commitment(bob_own);
+  const bob_tx_hints_bag = Loader.Ergo.TransactionHintsBag.empty();
+  bob_tx_hints_bag.add_hints_for_input(0, bob_hints_bag);
+  const signed_tx = wallet_bob.sign_transaction_multi(
+    ctx,
+    tx,
+    unspent_boxes,
+    tx_data_inputs,
+    bob_tx_hints_bag
+  );
+}
+/* eslint-enable */
