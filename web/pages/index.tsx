2e14e25acaa18be7e97bf7cae30a53cacf25452e
Daniel Oravec <danoravec@gmail.com>
2023-02-08 21:08:52 +0100

    Add footer

--- web/pages/index.tsx
+++ web/pages/index.tsx
@@ -2,12 +2,14 @@ import { Nav } from '@components/Nav/Nav';
 import { ThemeProvider } from 'styled-components';
 import { useThemeStore } from '@components/hooks';
 import React from 'react';
+import { Footer } from '@components/Footer/Footer';
 
 export default function Home(): JSX.Element {
   const { theme } = useThemeStore();
   return (
     <ThemeProvider theme={theme}>
       <Nav />
+      <Footer />
     </ThemeProvider>
   );
 }
