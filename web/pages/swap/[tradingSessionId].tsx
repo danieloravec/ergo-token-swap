f25c83ff78841eb605fffd2ca3bde7e3ca7f9802
Daniel Oravec <danoravec@gmail.com>
2023-02-09 16:30:05 +0100

    Conditional rendering of Swap parts in progress
    * Getting CORS errors from BE for now.

--- web/pages/swap/[tradingSessionId].tsx
+++ web/pages/swap/[tradingSessionId].tsx
@@ -1,33 +1,72 @@
 import { Nav } from '@components/Nav/Nav';
 import { ThemeProvider } from 'styled-components';
 import { useThemeStore } from '@components/hooks';
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Footer } from '@components/Footer/Footer';
 import { WaitingPhaseCreator } from '@components/Swap/WaitingPhaseCreator';
 import { useRouter } from 'next/router';
 // import {WaitingPhaseGuest} from "@components/Swap/WaitingPhaseGuest";
-// import {backendRequest} from "@utils/utils";
 import { useWalletStore } from '@components/Wallet/hooks';
 import { SwapWalletNotConnected } from '@components/Swap/SwapWalletNotConnected';
+import { backendRequest } from '@utils/utils';
+import { WaitingPhaseGuest } from '@components/Swap/WaitingPhaseGuest';
 
 export default function Swap(): JSX.Element {
+  const creatorRepr = 'creator';
+  const guestRepr = 'guest';
   const { theme } = useThemeStore();
   const { address } = useWalletStore();
+  const router = useRouter();
+  const { tradingSessionId } = router.query;
+  const [whoami, setWhoami] = useState<string | undefined>(undefined);
+  useEffect(() => {
+    const fetchWhoamiMaybeEnter = async (): Promise<void> => {
+      const whoamiResponse = await backendRequest(
+        `/whoami?secret=${tradingSessionId}&address=${address}`
+      );
+      if (whoamiResponse.status !== 200) {
+        setWhoami(undefined);
+      } else if (whoamiResponse.body?.whoami !== undefined) {
+        setWhoami(whoamiResponse.body.whoami);
+      } else if (address !== undefined) {
+        const sessionEnterBody = {
+          secret: tradingSessionId,
+          guestAddr: address,
+        };
+        const sessionEnterResponse = await backendRequest(
+          `/session/enter?secret=${tradingSessionId}&address=${address}`,
+          'POST',
+          sessionEnterBody
+        );
+        if (sessionEnterResponse.status !== 200) {
+          throw new Error('Error entering session as a guest.');
+        }
+        setWhoami(guestRepr);
+      }
+    };
+    fetchWhoamiMaybeEnter().catch(console.error);
+  }, [address, tradingSessionId]);
+
   if (address === undefined) {
     return <SwapWalletNotConnected />;
   }
-  const router = useRouter();
-  const { tradingSessionId } = router.query;
   if (typeof tradingSessionId !== 'string') {
     return <div>Invalid trading session id</div>; // TODO use something more reasonable here
   }
 
-  // const whoami = await backendRequest(`/whoami?secret=${tradingSessionId}&address=${address}`)
+  if (whoami !== creatorRepr && whoami !== guestRepr) {
+    return <div>Loading...</div>;
+  }
 
   return (
     <ThemeProvider theme={theme}>
       <Nav />
-      <WaitingPhaseCreator tradingSessionId={tradingSessionId} />
+      {whoami === creatorRepr && (
+        <WaitingPhaseCreator tradingSessionId={tradingSessionId} />
+      )}
+      {whoami === guestRepr && (
+        <WaitingPhaseGuest tradingSessionId={tradingSessionId} />
+      )}
       <Footer />
     </ThemeProvider>
   );
--- web/utils/utils.ts
+++ web/utils/utils.ts
@@ -1,9 +1,20 @@
 import { config } from '@config';
 
-export const backendRequest = async (endpoint: string): Promise<any> => {
+export const backendRequest = async (
+  endpoint: string,
+  method: string = 'GET',
+  body: any = undefined
+): Promise<any> => {
   if (!endpoint.startsWith('/')) {
     endpoint = `/${endpoint}`;
   }
-  const response = await fetch(`${config.backendUrl}${endpoint}`);
+  const response = await fetch(`${config.backendUrl}${endpoint}`, {
+    method,
+    headers: {
+      Accept: 'application/json',
+      'Content-Type': 'application/json',
+    },
+    body: body !== undefined ? JSON.stringify(body) : undefined,
+  });
   return await response.json();
 };
