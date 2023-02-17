72b6433937e3293a6d2e03d1f8ab318188f9fc1e
Daniel Oravec <danoravec@gmail.com>
2023-02-17 09:25:57 +0100

    Poll for guest connection from waiting room
    * The session creator is automatically
      redirected to the swap UI after the
      guest joins the session.

--- web/components/Swap/WaitingPhaseCreator.tsx
+++ web/components/Swap/WaitingPhaseCreator.tsx
@@ -6,17 +6,46 @@ import {
   TextPrimaryWrapper,
 } from '@components/Common/Text';
 import { Hourglass } from '@components/Icons/Hourglass';
-import React from 'react';
+import React, { useEffect } from 'react';
 import {
   CenteredDivHorizontal,
   CenteredDivVertical,
   MainSectionDiv,
 } from '@components/Common/Alignment';
+import { backendRequest } from '@utils/utils';
+import { useRouter } from 'next/router';
 
 export function WaitingPhaseCreator(props: {
   tradingSessionId: string;
 }): JSX.Element {
   const theme = useTheme();
+  const router = useRouter();
+
+  useEffect(() => {
+    const tryFetchGuestInfo = async (): Promise<void> => {
+      let foundGuestInfo = false;
+      while (!foundGuestInfo) {
+        try {
+          const sessionInfoResponse = await backendRequest(
+            `/session/info?secret=${props.tradingSessionId}`,
+            'GET'
+          );
+          if (sessionInfoResponse.status !== 200) {
+            console.error('Failed to get session info');
+          }
+          if (sessionInfoResponse?.body?.guest !== undefined) {
+            foundGuestInfo = true;
+          }
+        } catch (err) {
+          console.error(err);
+        }
+        await new Promise((resolve) => setTimeout(resolve, 1000));
+      }
+      router.reload();
+    };
+    tryFetchGuestInfo().catch(console.error);
+  });
+
   return (
     <ThemeProvider theme={theme}>
       <MainSectionDiv>
