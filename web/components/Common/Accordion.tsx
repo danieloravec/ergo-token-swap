718dcdba64abead8dd0eaaa28404414a89179d36
Daniel Oravec <danoravec@gmail.com>
2023-02-18 17:15:15 +0100

    [FE] Homepage (#26)
    * Add Accordion component.
    
    * Finalize homepage.
    
    * Implement start trading session button.
    
    * Fix footer covering content on small screen.
    
    * Add beast image.

---
+++ web/components/Common/Accordion.tsx
@@ -0,0 +1,84 @@
+import { CenteredDivVertical, FlexDiv } from '@components/Common/Alignment';
+import { Heading1, Heading3 } from '@components/Common/Text';
+import { useState } from 'react';
+import styled, { ThemeProvider, useTheme } from 'styled-components';
+
+interface AccordionProps {
+  width?: string;
+  title: string;
+  entries: Array<{
+    question: string;
+    answer: string;
+  }>;
+}
+
+const SvgMaybeRotated = styled.svg<{ rotated: boolean }>`
+  transform: ${(props) => (props.rotated ? 'rotate(90deg)' : 'rotate(0deg)')};
+`;
+
+const Arrow = (props: { rotated: boolean }): JSX.Element => {
+  return (
+    <SvgMaybeRotated
+      rotated={props.rotated}
+      xmlns="http://www.w3.org/2000/svg"
+      viewBox="0 0 6 10"
+      width="24"
+      height="24"
+    >
+      <path d="m1 0l5 5-5 5-1-1 4-4-4-4" />
+    </SvgMaybeRotated>
+  );
+};
+
+const AccordionContainer = styled.div<{ width?: string }>`
+  width: ${(props) => props.width ?? '100%'};
+  color: ${(props) => props.theme.properties.colorBgText};
+`;
+
+const AccordionItemWrapper = styled(FlexDiv)`
+  width: 100%;
+  border-bottom: ${(props) =>
+    `1px solid ${props.theme.properties.colorBgText}`};
+`;
+
+const HeadingArrowWrapper = styled(CenteredDivVertical)`
+  flex-direction: row;
+  align-items: center;
+  width: 100%;
+  justify-content: space-between;
+`;
+
+export const Accordion = (props: AccordionProps): JSX.Element => {
+  const theme = useTheme();
+  const [openIndex, setOpenIndex] = useState<number | null>(null);
+
+  return (
+    <ThemeProvider theme={theme}>
+      <AccordionContainer width={props.width}>
+        <Heading1>{props.title}</Heading1>
+        {props.entries.map((entry, idx) => {
+          return (
+            <AccordionItemWrapper
+              key={idx}
+              onClick={() => {
+                if (idx === openIndex) {
+                  setOpenIndex(null);
+                } else {
+                  setOpenIndex(idx);
+                }
+              }}
+            >
+              <HeadingArrowWrapper>
+                <Heading3>{entry.question}</Heading3>
+                <Arrow rotated={idx === openIndex} />
+              </HeadingArrowWrapper>
+              {idx === openIndex && (
+                <p style={{ width: '100%' }}>{entry.answer}</p>
+              )}
+            </AccordionItemWrapper>
+          );
+        })}
+      </AccordionContainer>
+    </ThemeProvider>
+  );
+};
--- web/pages/index.tsx
+++ web/pages/index.tsx
@@ -3,12 +3,70 @@ import { ThemeProvider } from 'styled-components';
 import { useThemeStore } from '@components/hooks';
 import React from 'react';
 import { Footer } from '@components/Footer/Footer';
+import { Accordion } from '@components/Common/Accordion';
+import { FlexDiv, MainSectionDiv } from '@components/Common/Alignment';
+import { Introduction } from '@components/Home/Introduction';
+import Image from 'next/image';
+import beastImage from '@public/token-swap-beast.png';
 
 export default function Home(): JSX.Element {
   const { theme } = useThemeStore();
   return (
     <ThemeProvider theme={theme}>
       <Nav />
+      <MainSectionDiv style={{ justifyContent: 'space-evenly' }}>
+        <FlexDiv>
+          <FlexDiv style={{ width: '50%' }}>
+            <Introduction />
+            <Accordion
+              width="600px"
+              title="FAQ"
+              entries={[
+                {
+                  question: 'What is this?',
+                  answer:
+                    'A multisig trustless escrow service on Ergo. If you want to swap some NFTs and tokens with ' +
+                    'someone you met on Discord, you can start a trading session here and send them a link to your private ' +
+                    'trading room.',
+                },
+                {
+                  question: 'Do you use smart contracts?',
+                  answer:
+                    "We don't need them! The swap happens in a single transaction that both parties sign.",
+                },
+                {
+                  question: 'Is this secure?',
+                  answer:
+                    'Yes! There are basically no risks if you carefully check the transaction before signing it. ' +
+                    'Once the transaction is submitted, the swap happens as a whole or does not happen at all if it fails.',
+                },
+                {
+                  question: 'Are there any fees?',
+                  answer:
+                    "We are in a testing phase where we don't charge any fees. We will add a small fee in the future.",
+                },
+                {
+                  question: 'Can I see the source code?',
+                  answer:
+                    'Of course! The open-source nature of Ergo helped us a lot and we want to move the ecosystem forward. ' +
+                    'Our GitHub is linked in the footer.',
+                },
+              ]}
+            />
+          </FlexDiv>
+          <div
+            style={{ position: 'relative', width: '50%', aspectRatio: '1 / 1' }}
+          >
+            <Image
+              fill
+              src={beastImage}
+              placeholder="blur"
+              alt="Token swapping transaction"
+              sizes="(max-width: 768px) 100vw, 50vw"
+            />
+          </div>
+        </FlexDiv>
+      </MainSectionDiv>
       <Footer />
     </ThemeProvider>
   );
