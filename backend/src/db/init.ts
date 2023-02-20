2255140a8afe71059a68ad7b306b4acba1752ba2
Daniel Oravec <danoravec@gmail.com>
2023-02-20 17:53:08 +0100

    Only leave sync force when debug

--- backend/src/db/init.ts
+++ backend/src/db/init.ts
@@ -1,4 +1,4 @@
 import Session from '@db/models/session';
 import {config} from '@config';
 
-Session.sync({force: config.debug, alter: config.debug === true ? undefined : true});
+Session.sync({force: config.debug});
