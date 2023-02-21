c7ea5623d131eaa949158f99e4ad76eddb82afe5
Daniel Oravec <danoravec@gmail.com>
2023-02-21 21:12:09 +0100

    [FE] Default theme (#41)
    * Try to wait for the theme to load
    
    * Use default theme until selected theme is loaded

--- web/pages/_app.tsx
+++ web/pages/_app.tsx
@@ -1,10 +1,12 @@
 import '../styles/globals.css';
 import type { AppProps } from 'next/app';
 import { useWalletConnect } from '@components/Wallet/hooks';
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import { Chakra_Petch } from '@next/font/google';
 import { ThemeProvider } from 'styled-components';
 import { useThemeStore } from '@components/hooks';
+import { config } from '@config';
+import { themes } from '@themes/themes';
 
 const chakraPetch = Chakra_Petch({
   weight: '500',
@@ -13,6 +15,11 @@ const chakraPetch = Chakra_Petch({
 
 export default function App({ Component, pageProps }: AppProps): JSX.Element {
   const { reconnect } = useWalletConnect();
+  const [selectedThemeName, setSelectedThemeName] = useState<'light' | 'dark'>(
+    config.defaultThemeName
+  );
+  const { themeName } = useThemeStore();
+
   useEffect(() => {
     const load = async (): Promise<void> => {
       await reconnect();
@@ -20,11 +27,13 @@ export default function App({ Component, pageProps }: AppProps): JSX.Element {
     void load();
   }, []);
 
-  const { theme } = useThemeStore();
+  useEffect(() => {
+    setSelectedThemeName(themeName);
+  });
 
   return (
     <main className={chakraPetch.className}>
-      <ThemeProvider theme={theme}>
+      <ThemeProvider theme={themes[selectedThemeName]}>
         <Component {...pageProps} />
       </ThemeProvider>
     </main>
