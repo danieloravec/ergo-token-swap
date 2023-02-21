239189aa3b6302ec3de3af4d6e5979b579b36f0e
Daniel Oravec <danoravec@gmail.com>
2023-02-21 21:03:56 +0100

    Try to wait for the theme to load

--- web/pages/_app.tsx
+++ web/pages/_app.tsx
@@ -5,6 +5,8 @@ import { useEffect } from 'react';
 import { Chakra_Petch } from '@next/font/google';
 import { ThemeProvider } from 'styled-components';
 import { useThemeStore } from '@components/hooks';
+import { config } from '@config';
+import { themes } from '@themes/themes';
 
 const chakraPetch = Chakra_Petch({
   weight: '500',
@@ -13,6 +15,9 @@ const chakraPetch = Chakra_Petch({
 
 export default function App({ Component, pageProps }: AppProps): JSX.Element {
   const { reconnect } = useWalletConnect();
+  const { theme, setThemeName, setTheme } = useThemeStore();
+  const defaultTheme = themes[config.defaultThemeName];
+
   useEffect(() => {
     const load = async (): Promise<void> => {
       await reconnect();
@@ -20,11 +25,16 @@ export default function App({ Component, pageProps }: AppProps): JSX.Element {
     void load();
   }, []);
 
-  const { theme } = useThemeStore();
+  useEffect(() => {
+    if (theme === undefined) {
+      setThemeName(config.defaultThemeName);
+      setTheme(defaultTheme);
+    }
+  });
 
   return (
     <main className={chakraPetch.className}>
-      <ThemeProvider theme={theme}>
+      <ThemeProvider theme={theme === undefined ? defaultTheme : theme}>
         <Component {...pageProps} />
       </ThemeProvider>
     </main>
