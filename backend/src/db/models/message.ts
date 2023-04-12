2a73d7725e63054944fac65e7da802937e80d61b
Daniel Oravec <danoravec@gmail.com>
2023-04-12 10:46:13 +0200

    Add table for messages

---
+++ backend/src/db/models/message.ts
@@ -0,0 +1,45 @@
+import {DataTypes, Model} from 'sequelize'
+import sequelizeConnection from '@db/config'
+
+interface MessageAttributes {
+  fromAddress: string;
+  toAddress: string;
+  text: string;
+  seen: boolean;
+}
+
+class Message extends Model<MessageAttributes> implements MessageAttributes {
+  fromAddress: string;
+  toAddress: string;
+  text: string;
+  seen: boolean;
+}
+
+Message.init({
+  fromAddress: {
+    type: DataTypes.STRING(51),
+  },
+  toAddress: {
+    type: DataTypes.STRING(51),
+  },
+  text: {
+    type: DataTypes.STRING(1000),
+  },
+  seen: {
+    type: DataTypes.BOOLEAN,
+  },
+}, {
+  timestamps: true,
+  underscored: true,
+  sequelize: sequelizeConnection,
+  indexes: [
+    {
+      fields: ['fromAddress'],
+    },
+    {
+      fields: ['toAddress'],
+    }
+  ],
+})
+
+export default Message;
