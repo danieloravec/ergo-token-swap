05059d2af46c2dbfc7e998b55df5e16498515cce
Daniel Oravec <danoravec@gmail.com>
2023-09-03 07:24:19 +0200

    [WIP] Design collection page

--- backend/src/api/tx_router.ts
+++ backend/src/api/tx_router.ts
@@ -242,32 +242,11 @@ txRouter.post('/mint/build', async (req, res) => {
     res.status(200);
     res.send(mintTxInfo);
   } catch (err) {
-    console.error(`Error while minting the token: ${err} / ${JSONBig.stringify(err)}`);
+    console.error(`Error while minting the token: ${err}`);
     res.status(500);
-    res.send({message: "Server-side error while minting the token"});
+    res.send({message: `Server-side error while minting the token: ${err}`});
   }
 });
-
-
-// fetch("https://graphql.erg.zelcore.io/", {
-//   "headers": {
-//     "accept": "application/graphql-response+json, application/graphql+json, application/json, text/event-stream, multipart/mixed",
-//     "accept-language": "en-US,en;q=0.9,sk;q=0.8",
-//     "content-type": "application/json",
-//     "sec-ch-ua": "\"Chromium\";v=\"116\", \"Not)A;Brand\";v=\"24\", \"Brave\";v=\"116\"",
-//     "sec-ch-ua-mobile": "?0",
-//     "sec-ch-ua-platform": "\"Linux\"",
-//     "sec-fetch-dest": "empty",
-//     "sec-fetch-mode": "cors",
-//     "sec-fetch-site": "cross-site",
-//     "sec-gpc": "1"
-//   },
-//   "referrerPolicy": "strict-origin-when-cross-origin",
-//   "body": "{\"operationName\":\"Mutation\",\"query\":\"mutation Mutation($signedTransaction: SignedTransaction!) {\\n  submitTransaction(signedTransaction: $signedTransaction)\\n}\",\"variables\":{\"signedTransaction\":{\"dataInputs\":[],\"id\":\"54e347cf8a22b1bc2a30612c3993951de474c3196d59f15bb7b17f27f253dc77\",\"inputs\":[{\"boxId\":\"d7d27b051c822eb1dc899e581e32bd37526446258f977e3f8246bcbf6d010b0d\",\"spendingProof\":{\"extension\":{},\"proofBytes\":\"f2ec76f883e9106e0145b8ac6f567e7e2089d75dddec6079574abe212f7f0c89491bdd981fecdeb1e48ffd056b93c97c3c3a0bdf7db1dc8e\"}},{\"boxId\":\"7a3c7416c0fb4ad4b87d6dfaab90b74131a764fb8bd2e7b62be2511475286c67\",\"spendingProof\":{\"extension\":{},\"proofBytes\":\"54f2e355c80ee02adab414511aaa5a6861d3908affc1d5da1ab2a9ab9eadea5c84f898b92d9e7ae52d976c066eadcb693d2a82aceeb54538\"}},{\"boxId\":\"c590fcf704b84d1677cc033a6e7dee99c7035c2c2f55b5da3f3f254bca696a55\",\"spendingProof\":{\"extension\":{},\"proofBytes\":\"2b9ae7c9799f64a322f89df173367725d9a61bcb46e9bf087b3edb994958f713093f72473557c4b84b293a4de879b7574ca9393ee649b207\"}}],\"outputs\":[{\"additionalRegisters\":{},\"assets\":[],\"boxId\":\"2cdda4bd3966e7a89983b7e08118f095d05c08adc064c7d824af4557f68660e5\",\"creationHeight\":1082219,\"ergoTree\":\"0008cd023d4d759cb6e556ed39d963ee06b1b2e5c7ebf8ae00181f9a5c287579f641d42e\",\"index\":0,\"transactionId\":\"54e347cf8a22b1bc2a30612c3993951de474c3196d59f15bb7b17f27f253dc77\",\"value\":\"100000000\"},{\"additionalRegisters\":{},\"assets\":[],\"boxId\":\"141dcfe8ffd4174b2c6bab75256fc39705a58754fd32fcf3469510e15e0e816e\",\"creationHeight\":1082219,\"ergoTree\":\"1005040004000e36100204a00b08cd0279be667ef9dcbbac55a06295ce870b07029bfcdb2dce28d959f2815b16f81798ea02d192a39a8cc7a701730073011001020402d19683030193a38cc7b2a57300000193c2b2a57301007473027303830108cdeeac93b1a57304\",\"index\":1,\"transactionId\":\"54e347cf8a22b1bc2a30612c3993951de474c3196d59f15bb7b17f27f253dc77\",\"value\":\"1100000\"},{\"additionalRegisters\":{},\"assets\":[{\"amount\":\"1\",\"tokenId\":\"536a2346778dd129d65e0edf5fefe63116644c5d4512c539e20e4fea6554f33b\"},{\"amount\":\"1\",\"tokenId\":\"a781688ca180d614171f9f34ab6e4a0cf0c402e4c7744b34c27906581f93bfaf\"},{\"amount\":\"1\",\"tokenId\":\"70f7a5e08540424304c83618e5fecefcfbc0fc8dc7544e9a69feedf5d99b48ef\"},{\"amount\":\"6904\",\"tokenId\":\"fbbaac7337d051c10fc3da0ccb864f4d32d40027551e1c3ea3ce361f39b91e40\"},{\"amount\":\"705647953882\",\"tokenId\":\"30974274078845f263b4f21787e33cc99e9ec19a17ad85a5bc6da2cca91c5a2e\"},{\"amount\":\"1\",\"tokenId\":\"747d76efd075d9c406f44082359284f51bd8806a7bd346a4cef7ab99f1c3a84a\"},{\"amount\":\"1\",\"tokenId\":\"c22823a2f1e4879d36f494ad8cd2be8ce5a2e0d85f36934c33b759f2328e932a\"},{\"amount\":\"943499952\",\"tokenId\":\"1c51c3a53abfe87e6db9a03c649e8360f255ffc4bd34303d30fc7db23ae551db\"},{\"amount\":\"3500\",\"tokenId\":\"e91cbc48016eb390f8f872aa2962772863e2e840708517d1ab85e57451f91bed\"},{\"amount\":\"1045314192\",\"tokenId\":\"e249780a22e14279357103749102d0a7033e0459d10b7f277356522ae9df779c\"},{\"amount\":\"4\",\"tokenId\":\"36aba4b4a97b65be491cf9f5ca57b5408b0da8d0194f30ec8330d1e8946161c1\"},{\"amount\":\"16358673780379\",\"tokenId\":\"ef802b475c06189fdbf844153cdc1d449a5ba87cce13d11bb47b5a539f27f12b\"},{\"amount\":\"6000\",\"tokenId\":\"0779ec04f2fae64e87418a1ad917639d4668f78484f45df962b0dec14a2591d2\"},{\"amount\":\"1\",\"tokenId\":\"4a1d257931b71dc79b5bd5e4d2d3800795e565dff3c6fc9772d8df12379f0925\"}],\"boxId\":\"1cc34d621c1a46b2656e742ae6a77cb98ec8458c2557977520b5f376ee32a6a9\",\"creationHeight\":1082219,\"ergoTree\":\"0008cd023d4d759cb6e556ed39d963ee06b1b2e5c7ebf8ae00181f9a5c287579f641d42e\",\"index\":2,\"transactionId\":\"54e347cf8a22b1bc2a30612c3993951de474c3196d59f15bb7b17f27f253dc77\",\"value\":\"1374305535\"}]}}}",
-//   "method": "POST",
-//   "mode": "cors",
-//   "credentials": "omit"
-// });
 
 txRouter.post('/mint/submit', async (req, res) => {
   try {
@@ -278,22 +257,18 @@ txRouter.post('/mint/submit', async (req, res) => {
 
     const {status, message} = await submitTx(body.signedTx);
 
-    if (status !== 200) {
-      res.status(status);
-      res.send({message});
-      return;
+    if (status === 200) {
+      await Reward.update({
+        giveaway_tx_submitted_at: new Date(),
+      }, {
+        where: {
+          giveaway_tx_id: message,
+        }
+      });
     }
 
-    await Reward.update({
-      giveaway_tx_submitted_at: new Date(),
-    }, {
-      where: {
-        giveaway_tx_id: txId,
-      }
-    });
-
     res.status(status);
-    res.send({ message }); // TODO test this
+    res.send({ message });
   } catch (err) {
     res.status(500);
     console.error(`Error while submitting minting tx: ${err}`);
--- web/pages/nft-collection.tsx
+++ web/pages/nft-collection.tsx
@@ -1,7 +1,10 @@
-import { FlexDiv } from '@components/Common/Alignment';
+import {
+  CenteredDivHorizontal,
+  FlexDivRow,
+} from '@components/Common/Alignment';
 import { Button } from '@components/Common/Button';
 import { backendRequest } from '@utils/utils';
-import { Input } from '@components/Common/Text';
+import { A, Heading1, Input } from '@components/Common/Text';
 import { useState } from 'react';
 import { useWalletStore } from '@components/Wallet/hooks';
 import {
@@ -11,38 +14,65 @@ import {
   type SignedTransaction,
 } from '@fleet-sdk/common';
 import { type Amount, type Box } from '@fleet-sdk/core';
-import JSONBig from 'json-bigint';
+import { Alert } from '@components/Common/Alert';
+import { config } from '@config';
+import { useTheme } from 'styled-components';
+
+const NftCollectionHeading = (): JSX.Element => {
+  const theme = useTheme();
+  return (
+    <FlexDivRow>
+      <CenteredDivHorizontal>
+        {/* <Heading1 style={{fontSize: "60px", color: theme.properties.colorSecondary}}>UTxO Beastz</Heading1> */}
+        <Heading1 style={{ fontSize: '7em' }}>
+          <span style={{ color: theme.properties.colorSecondary }}>UTxO</span>{' '}
+          Beastz
+        </Heading1>
+      </CenteredDivHorizontal>
+    </FlexDivRow>
+  );
+};
 
 const NftCollection = (): JSX.Element => {
   const [mintAmount, setMintAmount] = useState<number>(0);
+  const [submittedTxId, setSubmittedTxId] = useState<string | undefined>(
+    undefined
+  );
+  const [error, setError] = useState<string | undefined>(undefined);
   const { wallet, address } = useWalletStore();
 
+  interface BuildMintTxBodySuccess {
+    unsignedTx: EIP12UnsignedTransaction;
+    signedMintInputs: SignedInput[];
+    inputIndicesMint: number[];
+  }
+
   const handleMint = (amountToMint: number): void => {
+    setError(undefined);
+    setSubmittedTxId(undefined);
+
     if (amountToMint === 0 || wallet === undefined || address === undefined) {
       return;
     }
     const mintNft = async (amountToMint: number): Promise<void> => {
       const mintingTxInfo: {
         status: number;
-        body?: {
-          unsignedTx: EIP12UnsignedTransaction;
-          signedMintInputs: SignedInput[];
-          inputIndicesMint: number[];
-        };
+        body?: BuildMintTxBodySuccess | { message: string };
       } = await backendRequest('/tx/mint/build', 'POST', {
         address,
         amount: amountToMint,
       });
 
-      if (mintingTxInfo.status !== 200 || mintingTxInfo.body === undefined) {
-        throw new Error(
-          `Error minting NFT, backend returned this: ${JSONBig.stringify(
-            mintingTxInfo
-          )}`
+      if (mintingTxInfo.status !== 200) {
+        setError(
+          `Error minting NFT: ${
+            (mintingTxInfo.body as { message: string }).message
+          }`
         );
+        return;
       }
 
-      const body = mintingTxInfo.body;
+      const body = mintingTxInfo.body as BuildMintTxBodySuccess;
 
       const mintInputsErgoTree =
         body.unsignedTx.inputs[body.inputIndicesMint[0]].ergoTree;
@@ -83,12 +113,16 @@ const NftCollection = (): JSX.Element => {
         }),
       };
 
-      const submittedTxId = await backendRequest('/tx/mint/submit', 'POST', {
+      const submitTxResult = await backendRequest('/tx/mint/submit', 'POST', {
         type: 'mint',
         signedTx,
       });
 
-      console.log(`submitted minting tx with id: ${submittedTxId}`);
+      if (submitTxResult.status === 200) {
+        setSubmittedTxId(submitTxResult.body.message);
+      } else {
+        setError(submitTxResult.body.message);
+      }
     };
     mintNft(amountToMint).catch(console.error);
   };
@@ -98,7 +132,25 @@ const NftCollection = (): JSX.Element => {
   };
 
   return (
-    <FlexDiv>
+    <FlexDivRow>
+      <NftCollectionHeading />
+      {error !== undefined && (
+        <Alert type="error" marginSides="0px">
+          {error}
+        </Alert>
+      )}
+      {submittedTxId !== undefined && (
+        <Alert type="success" marginSides="0px">
+          Successfully submitted a minting transaction. You can view it in the{' '}
+          <A
+            target="_blank"
+            href={`${config.explorerFrontendUrl}/transactions/${submittedTxId}`}
+          >
+            explorer
+          </A>
+          .
+        </Alert>
+      )}
       <Input type="number" onChange={handleAmountChange} />
       <Button
         onClick={(e) => {
@@ -107,7 +159,7 @@ const NftCollection = (): JSX.Element => {
       >
         Mint NFT
       </Button>
-    </FlexDiv>
+    </FlexDivRow>
   );
 };
 
